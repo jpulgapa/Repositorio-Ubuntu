074d9cd15e5ac71b75622efc86203dff68727036
jpulgapa <Sebas465yt@gmail.com>
2025-10-02 20:08:56 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Iniciando aplicación CRUD en modo PRODUCCIÓN..."
-# Llama a 'make up' del Makefile.prod, que usa SOLO el archivo 'docker-compose.yml'.
-make -f Makefile.prod up
-echo "Aplicación de PRODUCCIÓN iniciada. Verifica la configuración de puertos."
