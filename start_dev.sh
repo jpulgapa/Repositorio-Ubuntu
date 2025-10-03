83c81275b2dcf23f2f68f3824788bc0e9c183c1c
jpulgapa <Sebas465yt@gmail.com>
2025-10-02 20:08:38 -0500

    Delete start_dev.sh

--- start_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Iniciando aplicación CRUD..."
-make up
-echo "Aplicación iniciada. Accede a:"
-echo "Frontend: http://localhost:3005"
-echo "Backend: http://localhost:8080"
-echo "Base de datos MySQL en el contenedor 'mysql' en el puerto 3306."
-echo "Para detener la aplicación, usa 'make down'."
